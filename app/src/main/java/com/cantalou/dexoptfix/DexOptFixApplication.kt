e630cee635624dcbec7972ff795c98f37dd3a0b1
linzhiwei <linzhiwei1@4399inc.com>
2019-09-03 16:16:24 +0800

    add readme

--- app/src/main/java/com/cantalou/dexoptfix/DexOptFixApplication.kt
+++ app/src/main/java/com/cantalou/dexoptfix/DexOptFixApplication.kt
@@ -9,7 +9,6 @@ import android.support.multidex.MultiDex
  * @author  LinZhiWei
  * @date    2019年08月30日 12:03
  *
- * Copyright (c) 2019年, 4399 Network CO.ltd. All Rights Reserved.
  */
 class DexOptFixApplication : Application() {
 
